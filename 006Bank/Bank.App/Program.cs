da1c0b59edef0edccd63930102f20eee749f830a
Simon Koller <113828769+shotgun007@users.noreply.github.com>
2023-10-07 12:13:27 +0200

    Add files via upload

---
+++ 006Bank/Bank.App/Program.cs
@@ -0,0 +1,49 @@
+﻿using Bank.Logic;
+
+Account bank;
+Console.Write("Type of account ([c]hecking, [b]usiness, [s]avings): ");
+string accountType = Console.ReadLine()!;
+
+Console.Write("Enter your account number: ");
+string accountNumber = Console.ReadLine()!;
+Console.Write("Enter the account holder: ");
+string accountHolder = Console.ReadLine()!;
+Console.Write("Enter your current balance: ");
+decimal currentBalance = decimal.Parse(Console.ReadLine()!);
+Console.Write("Enter the account of the transaction: ");
+string tAccountNumber = Console.ReadLine()!;
+Console.Write("Enter a description of the transaction: ");
+string description = Console.ReadLine()!;
+Console.Write("Enter the amount of the transaction: ");
+decimal amount = decimal.Parse(Console.ReadLine()!);
+Console.Write("Enter the transaction timestamp: ");
+DateTime time = DateTime.Parse(Console.ReadLine()!);
+
+var transaction = new Bank.Logic.Transaction(tAccountNumber, description, time, amount);
+switch (accountType)
+{
+    case "c":
+        bank = new CheckingAccount(accountNumber, accountHolder, currentBalance);
+        bank.IsAllowed(transaction);
+        bank.TryExecute(transaction);
+        break;
+    case "b":
+        bank = new BusinessAccount(accountNumber, accountHolder, currentBalance);
+        bank.IsAllowed(transaction);
+        bank.TryExecute(transaction);
+        break;
+    case "s":
+        bank = new Savings(accountNumber, accountHolder, currentBalance);
+        bank.IsAllowed(transaction);
+        bank.TryExecute(transaction);
+        break;
+    default:
+        Console.WriteLine("ERROR! Invalid account type.");
+        break;
+}
+
+
+
+
+
+
---
+++ 006Bank/Bank.Logic/Account.cs
@@ -0,0 +1,156 @@
+﻿namespace Bank.Logic;
+public abstract class Account
+{
+    public Account(string accountNumber, string accountHolder, decimal currentBalance)
+    {
+        AccountNumber = accountNumber;
+        AccountHolder = accountHolder;
+        CurrentBalance = currentBalance;
+    }
+    public string AccountNumber { get; }
+    public string AccountHolder { get; }
+    public decimal CurrentBalance { get; set; }
+
+    public abstract bool IsAllowed(Transaction t);
+    public bool TryExecute(Transaction t)
+    {
+        if (IsAllowed(t))
+        {
+            CurrentBalance = CurrentBalance + t.Amount;
+            Console.WriteLine($"Transaction executed successfully. The new current balance is {CurrentBalance}$.");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Transaction not allowed.");
+            return false;
+        }
+    }
+    
+}
+
+public class Savings : Account
+{
+    public Savings(string accountNumber, string accountHolder, decimal currentBalance)
+        : base(accountNumber, accountHolder, currentBalance) { }
+
+    public override bool IsAllowed(Transaction t)
+    {
+        if (t.TAccountNumber == AccountNumber)
+        {
+            if (CurrentBalance + t.Amount >= 0)
+            {
+                if (CurrentBalance + t.Amount <= 100000000)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+public class CheckingAccount : Account
+{
+    public CheckingAccount(string accountNumber, string accountHolder, decimal currentBalance)
+        : base(accountNumber, accountHolder, currentBalance) { }
+    public override bool IsAllowed(Transaction t)
+    {
+        if (t.TAccountNumber == AccountNumber)
+        {
+            if (CurrentBalance + t.Amount >= -10000)
+            {
+                if (CurrentBalance + t.Amount <= 10000000)
+                {
+                    if (t.Amount <= 10000)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+public class BusinessAccount : Account
+{
+    public BusinessAccount(string accountNumber, string accountHolder, decimal currentBalance)
+        : base(accountNumber, accountHolder, currentBalance) { }
+    public override bool IsAllowed(Transaction t)
+    {
+        if (t.TAccountNumber == AccountNumber)
+        {
+            if (CurrentBalance + t.Amount >= -1000000)
+            {
+                if (CurrentBalance + t.Amount <= 100000000)
+                {
+                    if (t.Amount <= 100000)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+
+
+
+public class Transaction
+{
+    public Transaction(string tAccountNumber, string description, DateTime time, decimal amount)
+    {
+        TAccountNumber = tAccountNumber;
+        Description = description;
+        Time = time;
+        Amount = amount;
+    }
+    public string TAccountNumber { get; set; }
+    public string Description { get; set; }
+    public DateTime Time { get; set; }
+    public decimal Amount { get; set; }
+}
